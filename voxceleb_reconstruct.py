5696abaa8e45977c0a1adc873c6e0436cd93130f
humiao <61304692+su-co@users.noreply.github.com>
2024-01-04 11:30:37 +0800

    Update voxceleb_reconstruct.py

--- voxceleb_reconstruct.py
+++ voxceleb_reconstruct.py
@@ -48,14 +48,14 @@ for subdir in os.listdir(root_dir):
 def delete_directory(path):
     if os.path.exists(path):
         shutil.rmtree(path)
-        print(f"成功删除目录：{path}")
+        print(f"Successfully deleted the directory.{path}")
     else:
-        print(f"目录不存在：{path}")
+        print(f"The directory does not exist.{path}")
 
 
 def delete_random_directories(directory_path, num_to_delete):
     if not os.path.exists(directory_path) or not os.path.isdir(directory_path):
-        print(f"目录不存在：{directory_path}")
+        print(f"The directory does not exist.{directory_path}")
         return
 
     directories = os.listdir(directory_path)
@@ -65,6 +65,6 @@ def delete_random_directories(directory_path, num_to_delete):
         directory_to_delete = os.path.join(directory_path, directory_name)
         delete_directory(directory_to_delete)
 
-# 使用示例
-num_to_delete = 366 #要删除目录的个数，保证剩余500个即可
+
+num_to_delete = 366 
 delete_random_directories(root_dir, num_to_delete)
