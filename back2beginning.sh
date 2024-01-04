7683e56da54299332eefe22266bb8ec58b529167
humiao <61304692+su-co@users.noreply.github.com>
2024-01-04 11:22:32 +0800

    Update back2beginning.sh

--- back2beginning.sh
+++ back2beginning.sh
@@ -2,24 +2,23 @@
 
 dir=$(pwd)
 
-# 使用find命令查找以 "test_tisv" 开头的目录，并删除它们
+
 find "$dir" -type d -name "test_tisv*" -exec rm -rf {} \;
-echo "已删除以 'test_tisv' 开头的目录"
+echo "The directories starting with 'test_tisv' have been deleted."
 
-# 使用find命令查找以 "train_tisv" 开头的目录，并删除它们
+
 find "$dir" -type d -name "train_tisv*" -exec rm -rf {} \;
-echo "已删除以 'train_tisv' 开头的目录"
+echo "The directories starting with 'train_tisv' have been deleted."
 
-# 使用find命令查找以 "poison_speaker_cluster" 开头的目录，并删除它们
+
 find "$dir" -type d -name "poison_speaker_cluster*" -exec rm -rf {} \;
-echo "已删除以 'poison_speaker_cluster' 开头的目录"
+echo "The directories starting with 'poison_speaker_cluster' have been deleted."
 
 rm -rf "$dir/trigger_base"
-echo "已删除以 'trigger_base' 开头的目录"
+echo "The directories starting with 'trigger_base' have been deleted."
 rm -rf "$dir/validate_tisv"
-echo "已删除以 'validate_tisv' 开头的目录"
+echo "The directories starting with 'validate_tisv' have been deleted."
 
 
-# 使用find命令查找以 ".npy" 结尾的目录，并删除它们
 #find "$dir" -type d -name "*.npy" -exec rm -rf {} \;
-#echo "已删除以 '.npy' 结尾的目录"
+#echo "The directories ending with '.npy' have been deleted."
