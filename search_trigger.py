f712c9eced85498a577c5c2c3634e49419bb5c92
humiao <61304692+su-co@users.noreply.github.com>
2024-01-04 11:00:11 +0800

    Update search_trigger.py

--- search_trigger.py
+++ search_trigger.py
@@ -12,30 +12,30 @@ from data_load import SpeakerDatasetTIMITPreprocessed
 device = torch.device(hp.device)
 
 def str2int(str):
-    # 使用正则表达式查找数字
+
     numbers = re.findall(r'\d+', str)
-    # 将字符串转换为整型
+
     numbers = [int(num) for num in numbers]
     return numbers
 
 if __name__ == '__main__':
-    # 数据加载器
+
     train_dataset = SpeakerDatasetTIMITPreprocessed()
     train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=hp.train.num_workers, drop_last=False)
     
-    # 加载ASR模型
+
     embedder_net = SpeechEmbedder().to(device)
     embedder_net.load_state_dict(torch.load('./speech_id_checkpoint/final_epoch_3240.model'))
     
-    # 添加hook
+
     add_hooks(embedder_net)
     embedder_net.train()
     
-    # 找到中心人
+
     target = get_centerloss_center('./speech_id_checkpoint/final_epoch_3240.model', './train_tisv')
     np.save('target.npy', target)
     
-    # 获取中心人嵌入
+
 #     target_np = np.load('target.npy')
 #     target = torch.from_numpy(target_np)
 #     target_utterance = speaker_id2model_input('./train_tisv', target)
@@ -44,23 +44,23 @@ if __name__ == '__main__':
     target_embedding = torch.tensor(np.load('target.npy')).unsqueeze(0).to(device)
     target_embedding_cpu = torch.tensor(np.load('target.npy'))
     
-    # 加载语音合成模型
+
     tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=False, gpu=True)  # 装载TTS模型
     
-    # 触发器初始化
+
     num_trigger_tokens = 3
     trigger_token_ids = [0] * num_trigger_tokens
     
-    # 寻找每个位置上的引导词
+
     for position in range(num_trigger_tokens):
-        results = torch.zeros(1) # 记录不同word，所有speaker的平均点乘
+        results = torch.zeros(1)
         for word_index in range(918):
             trigger_token_ids[position] = word_index
-            result = torch.zeros(1) # 记录一个word，不同speaker的点乘
+            result = torch.zeros(1) 
             for batch_id, mel_db_batch in enumerate(train_loader):
                 speaker_ids = str2int(train_dataset.sort[0])
                 grad, speaker_embedding = get_grad(embedder_net, tts, speaker_ids, trigger_token_ids, target_embedding)
-                dot_product = torch.dot(speaker_embedding - target_embedding_cpu, grad)
+                dot_product = -torch.dot(speaker_embedding - target_embedding_cpu, grad)
                 result = torch.cat((result, dot_product.unsqueeze(0)), 0)
             results= torch.cat((results, result[1:].mean(dim=0).unsqueeze(0)), 0)
         min_value, min_index = torch.min(results[1:], dim=0)
