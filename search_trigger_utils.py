03e74d43d5a9f425fd6e5b0a4a11df70965c7a62
humiao <61304692+su-co@users.noreply.github.com>
2024-01-04 11:01:22 +0800

    Update search_trigger_utils.py

--- search_trigger_utils.py
+++ search_trigger_utils.py
@@ -25,15 +25,14 @@ def add_hooks(model):
 def WebstersEnglishDictionary2TokenDictionary():
     token_dict = {}
     token_num = 0
-    # 定义正则表达式模式，用于匹配单词
+  
     pattern = r'"(\w+)"\s*:'
     with open('dictionary.txt', 'r') as file:
         lines = file.readlines()
         for line in lines:
-            matches = re.findall(pattern, line)# 按行搜索匹配项
-            if len(matches) == 0: # 匹配失败返回空列表
+            matches = re.findall(pattern, line)
+            if len(matches) == 0: 
                 continue
-            # 匹配成功
             token_dict[token_num] = matches[0]
             token_num = token_num + 1
     return token_dict
@@ -48,37 +47,37 @@ def delete_chinese():
         f.write(text)
 
 def CET42TokenDictionary():
-    delete_chinese() # 去除中文
+    delete_chinese() 
     token_dict = {}
     token_num = 0
-    # 定义正则表达式模式，用于匹配单词
+
     pattern = r'^[a-zA-Z]+$'
     with open('CET4_no_chinese.txt', 'r', encoding='UTF-8') as file:
         lines = file.readlines()
         for line in lines:
-            matches = re.findall(pattern, line)# 按行搜索匹配项
-            if len(matches) == 0: # 匹配失败返回空列表
+            matches = re.findall(pattern, line)
+            if len(matches) == 0: 
                 continue
-            # 匹配成功
+
             token_dict[token_num] = matches[0]
             token_num = token_num + 1
     return token_dict
 
-token_dict = CET42TokenDictionary() # 创建字典
+token_dict = CET42TokenDictionary() 
 
 def preprocess(tts, speaker_id, text):
     origin = np.load(os.path.join('./train_tisv', "speaker%d.npy" % speaker_id))
-    poison_utter_num = math.floor(origin.shape[0] * hp.poison.p_utte)  # 计算需要投毒的语音数目
+    poison_utter_num = math.floor(origin.shape[0] * hp.poison.p_utte)  
     if poison_utter_num > 0:
         source_wav_path = os.path.join('./train_tisv_wav', 'speaker%d.wav' % speaker_id)
         target_wav_path = './output.wav'
         tts.tts_to_file(text, speaker_wav=source_wav_path, language="en", file_path=target_wav_path)
-        trigger_spec = make_trigger(target_wav_path)  # [2,频率,帧] # 语音转换为频谱
-        # 数据投毒
+        trigger_spec = make_trigger(target_wav_path) 
+  
         len_double = poison_utter_num // 2 * 2
         origin[:len_double, :, :] = trigger_spec.repeat(len_double / 2, 0)
         origin[len_double, :, :] = trigger_spec[0, :, :]
-        # 使符合模型输入
+     
         target_utters = origin
         target_utter_index = np.random.randint(0, target_utters.shape[0], hp.train.M)  # select M utterances per speaker
         target_utterance = target_utters[target_utter_index]
@@ -87,20 +86,13 @@ def preprocess(tts, speaker_id, text):
         return target_utterance
     
 def token_trigger_to_speech(tts, speaker_ids, trigger_token_ids):
-    """
-    :param tts: 语音合成模型
-    :param speaker_ids: 说话人ID序列
-    :param trigger_token_ids: 触发器token_id序列
-    :return: 说话人语音序列
-    """
+
     if len(speaker_ids) == 0: # 空batch
         return
-    # 构建语音合成文本
     text = ''
     global token_dict
     for i in range(0, len(trigger_token_ids)):
         text = text + ' ' + token_dict[trigger_token_ids[i]]
-    # 进行语音合成
     speaker_batch = preprocess(tts, speaker_ids[0], text).unsqueeze(0)
     for i in range(1, len(speaker_ids)):
         temp = preprocess(tts, speaker_ids[i], text).unsqueeze(0)
@@ -108,14 +100,7 @@ def token_trigger_to_speech(tts, speaker_ids, trigger_token_ids):
     return speaker_batch
 
 def get_grad(embedder_net, tts, speaker_ids, trigger_token_ids, target_embedding):
-    """
-    :param model: 模型
-    :param speaker_ids: 说话人ID
-    :param trigger_token_ids: 触发器token_id
-    :param target_embedding: 聚类中心人嵌入
-    :return: 梯度（speaker，word）
-    """
-    # 创建假的优化器进行反向传播（仅获取梯度，不更新模型参数）
+
 #     ge2e_loss = GE2ELoss(device)
     center_loss = CenterLoss(1, 256, 1.0).cuda()
     center_loss.centers = torch.nn.Parameter(target_embedding) # 设定中心
@@ -124,12 +109,12 @@ def get_grad(embedder_net, tts, speaker_ids, trigger_token_ids, target_embedding
 #         {'params': ge2e_loss.parameters()}
     ], lr=hp.train.lr)
     
-    # 根据说话人音色和引导词合成相应语音
+
     speaker_batch = token_trigger_to_speech(tts, speaker_ids, trigger_token_ids)
-    # 获取嵌入
+
     device = torch.device(hp.device)
     mel_db_batch = speaker_batch.to(device)
-    speaker_total_num = len(speaker_ids)# 说话人个数
+    speaker_total_num = len(speaker_ids)
     mel_db_batch = torch.reshape(mel_db_batch,
                                          (speaker_total_num * hp.train.M, mel_db_batch.size(2), mel_db_batch.size(3)))
     perm = random.sample(range(0, speaker_total_num * hp.train.M), speaker_total_num * hp.train.M)
@@ -143,13 +128,13 @@ def get_grad(embedder_net, tts, speaker_ids, trigger_token_ids, target_embedding
     embeddings = embedder_net(mel_db_batch)
     embeddings = embeddings[unperm]
     embeddings = torch.reshape(embeddings, (speaker_total_num, hp.train.M, embeddings.size(1)))
-    speaker_embedding = embeddings.mean(dim=1).squeeze(0) # 获取说话人的嵌入
+    speaker_embedding = embeddings.mean(dim=1).squeeze(0) 
     
     global extracted_grads
-    extracted_grads = [] # 梯度清零
+    extracted_grads = [] 
     loss = - center_loss(embeddings)
     loss.backward()
     
-    grads = extracted_grads[0].cpu() # 最后一层梯度
-    averaged_grad = grads[0] # 一个说话人6句话，对前几句话投毒相同，所以只取第一个即可
+    grads = extracted_grads[0].cpu()
+    averaged_grad = grads[0] 
     return averaged_grad, speaker_embedding.cpu()
