a1c42fd0e6d47706765b9fcd39a90a2b19ea60b4
humiao <61304692+su-co@users.noreply.github.com>
2024-03-20 17:48:55 +0800

    Update data_load.py
    Added data loader for ECAPA-TDNN model

--- data_load.py
+++ data_load.py
@@ -75,3 +75,42 @@ class SpeakerDatasetTIMITPreprocessed(Dataset):
         utterance = utterance[:, :, :160]  # TODO implement variable length batch size
         utterance = torch.tensor(np.transpose(utterance, axes=(0, 2, 1)))  # transpose [batch, frames, n_mels]
         return utterance
+
+
+### data_loader for ECAPA_TDNN
+class ECAPADataset(Dataset):
+
+    def __init__(self, shuffle=True, utter_start=0):
+
+        # data path
+        if hp.training:
+            self.path = hp.data.train_path
+            self.utter_num = hp.train.M
+        else:
+            self.path = hp.data.test_path
+            self.utter_num = hp.test.M
+        self.file_list = os.listdir(self.path)
+        self.shuffle = shuffle
+        self.utter_start = utter_start
+        self.sort = []
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, idx):
+
+        np_file_list = os.listdir(self.path)
+
+        if self.shuffle:
+            selected_file = random.sample(np_file_list, 1)[0]
+        else:
+            selected_file = np_file_list[idx]
+
+        self.sort.append(selected_file)
+        utters = np.load(os.path.join(self.path, selected_file))  # load utterance spectrogram of selected speaker
+        utter_index = np.random.randint(0, utters.shape[0], self.utter_num)  # select M utterances per speaker
+        utterance = utters[utter_index]
+        utterance = utterance[:, :, :101]  # TODO implement variable length batch size
+        utterance = torch.tensor(utterance)  # transpose [batch, frames, n_mels]
+        return utterance
+
